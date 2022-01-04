4395eeb22dc4bfc83699b22847212a27a85f34a0
Rizwan Akram <rizwanakram4049@gmail.com>
2022-01-05 01:03:18 +0530

    feat: example of explicit ype annotation

--- src/variables.ts
+++ src/variables.ts
@@ -7,3 +7,19 @@ value = "this is book"
 
 // literal type { this cant be reassigned and changed }
 const age = 6
+
+
+// any type implicit
+
+// between 500 and 1000
+const RANDOM_WAIT_TIME =
+  Math.round(Math.random() * 500) + 500
+
+let startTime = new Date()
+let endTime : Date
+    
+setTimeout(() => {
+  endTime = 0
+  endTime = new Date()
+}, RANDOM_WAIT_TIME)
+
